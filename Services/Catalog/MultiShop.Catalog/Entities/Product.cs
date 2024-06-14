9b5bcfd3bcf52c533214f4cd7d2a2e2f0fd652c4
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-06-14 17:26:58 +0300

    Akın Özsoy
    MultiShop

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+	public class Product
+	{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }  // Ürünün Adı
+
+		public decimal ProductPrice { get; set; }  // Ürünün Fiyatı
+
+		public string ProductImageUrl { get; set; } // Ürünün Resmi
+
+        public string ProductDescription { get; set; } // Ürün Açıklaması
+
+        public string CategoryId { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+
+
+    }
+}

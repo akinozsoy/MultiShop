4b4d1c3bb25c33bdcb5ec64d02160263aa350237
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-07-16 17:58:20 +0300

    Updated

--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailQueryHandler.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailQueryHandler.cs
@@ -19,6 +19,22 @@ namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
 		}
 		public async Task<List<GetOrderDetailQueryResult>> Handle()
 		{
+			var values = await _repository.GetAllAsync();
+			return values.Select(x=> new GetOrderDetailQueryResult
+			{
+				OrderDetailId = x.OrderDetailId,
+				ProductAmount = x.ProductAmount,
+				OrderingId = x.OrderingId,
+				ProductId = x.ProductId,
+				ProductName = x.ProductName,
+				ProductPrice = x.ProductPrice,
+				ProductTotalPrice = x.ProductTotalPrice,
+
+			}).ToList();
+
+
+
+
 
 		}
 	}

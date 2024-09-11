81a24bef4a66e11b40929a41f646c89f17519e6b
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-09-11 16:47:49 +0300

    MultiShop
    Güncelleniyor

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCustomersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCustomerDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CargoCustomersController : ControllerBase
+	{
+		private readonly ICargoCustomerService _cargoCustomerService;
+
+		public CargoCustomersController(ICargoCustomerService cargoCustomerService)
+		{
+			_cargoCustomerService = cargoCustomerService;
+		}
+
+		[HttpGet]
+		public IActionResult CargoCustomerList()
+		{
+			var values = _cargoCustomerService.TGetAll();
+			return Ok(values);
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetCargoCustomersById(int id)
+		{
+			var value = _cargoCustomerService.TGetById(id);
+			return Ok(value);
+		}
+		[HttpPost]
+		public IActionResult CreateCargoCustomer(CreateCargoCustomerDto createCargoCustomerDto)
+		{
+			CargoCustomer cargoCustomer = new CargoCustomer()
+			{
+				Address = createCargoCustomerDto.Address,
+				City = createCargoCustomerDto.City,
+				District = createCargoCustomerDto.District,
+				Email = createCargoCustomerDto.Email,
+				Name = createCargoCustomerDto.Name,
+				Phone = createCargoCustomerDto.Phone,
+				SurName = createCargoCustomerDto.SurName,
+			};
+			_cargoCustomerService.TInsert(cargoCustomer);
+			return Ok("Kargo Müşteri Ekleme İşlemi Başarıyla Yapıldı");
+
+		}
+
+
+	}
+}

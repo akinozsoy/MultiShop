f9d6110643ea9130a7f7fd29484e7a9ed481989b
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-10-09 17:24:38 +0300

    AKIN ÖZSOY
    .

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,17 @@
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         public IActionResult Index()
         {
+            ViewBag.v0 = "Kategori İşlemleri";
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+           
+
             return View();
         }
     }

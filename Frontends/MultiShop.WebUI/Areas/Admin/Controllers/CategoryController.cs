b65b7eaf008930ed0d57a3c567f33ebf16447fa7
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-10-16 08:45:16 +0300

    MultiShop
    Güncelleniyor.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AllowAnonymous]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -14,13 +16,12 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
-            ViewBag.v0 = "Kategori İşlemleri";
-            ViewBag.v1 = "Anasayfa";
-            ViewBag.v2 = "Kategoriler";
-            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0 = "Anasayfa";
+            ViewBag.v1 = "Kategoriler";
+            ViewBag.v2 = "Kategori Listesi";
+            ViewBag.v3 = "Kategori İşlemleri";
 
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
@@ -30,7 +31,27 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
                 return View(values);
             }
-
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            ViewBag.v0 = "Anasayfa";
+            ViewBag.v1 = "Kategoriler";
+            ViewBag.v2 = "Yeni Kategori Girişi";
+            ViewBag.v3 = "Kategori İşlemleri";
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode) { 
+                return RedirectToAction("Index", "Category", new {area = "Admin"});
+            }
             return View();
         }
     }

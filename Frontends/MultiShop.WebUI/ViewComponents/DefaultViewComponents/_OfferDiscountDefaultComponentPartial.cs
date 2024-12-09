5a02ae6a51e7fc036db2c36bb15195d7c4ecc4ee
Akın Özsoy <akn.ozsy5757@gmail.com>
2024-12-09 20:22:07 +0300

    MultiShop
    Güncelleniyor

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
@@ -5,28 +5,28 @@ using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
-    public class _OfferDiscountDefaultComponentPartial : ViewComponent
-    {
+	public class _OfferDiscountDefaultComponentPartial : ViewComponent
+	{
 
-        private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IHttpClientFactory _httpClientFactory;
 
-        public _OfferDiscountDefaultComponentPartial(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+		public _OfferDiscountDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
 
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7075/api/OfferDiscounts");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultOfferDiscountDto>>(jsonData);
-                return View(values);
-            }
-            return View();
-        }
-    }
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7075/api/OfferDiscounts");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultOfferDiscountDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
